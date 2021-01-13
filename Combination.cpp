dc159ed0dc9387e14ec72616dde4c3bd799e378a
Kyungmo Ku <46951576+Kusin@users.noreply.github.com>
2021-01-14 00:22:26 +0900

    Update Combination.cpp

--- Combination.cpp
+++ Combination.cpp
@@ -3,6 +3,7 @@ using namespace std;
 using ll = long long;
 const int N = 1e6+5, mod = 1e9+7;
 inline int FastPow(int x,int n=mod-2){
+    if(x>=0 && x<=1) return x;
     int ans = 1;
     while(n>0){
         if(n&1)
