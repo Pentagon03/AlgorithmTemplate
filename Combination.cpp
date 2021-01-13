692394209d7f8d8b1114b2be5b4cd7733f62c317
Kyungmo Ku <46951576+Kusin@users.noreply.github.com>
2021-01-13 22:47:13 +0900

    Update Combination.cpp

--- Combination.cpp
+++ Combination.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 using ll = long long;
 const int mod = 1e9+7;
-int fac[1000001];
-int FastPow(int x,int n=mod-2){
+int startup=1,fac[1000001]={1};
+inline int FastPow(int x,int n=mod-2){
     int ans = 1;
     while(n>0){
         if(n&1)
@@ -13,14 +13,17 @@ int FastPow(int x,int n=mod-2){
     }
     return ans;
 }
-int nCr(int n,int r){
+inline int nCr(int n,int r){
+    if(startup){
+        for(int i=1;i<=n;i++)
+            fac[i] = 1LL*fac[i-1]*i%mod;
+        startup = 0;
+    }
     return 1LL*fac[n]*FastPow(1LL*fac[r]*fac[r]%mod)%mod;
 }
 int main(){
     ios::sync_with_stdio(!cin.tie(0));
     int n,r; cin>>n>>r;
-    fac[0]=1;
-    for(int i=1;i<=n;i++)
-        fac[i] = 1LL*fac[i-1]*i%mod;
+    
     cout<<nCr(n,r);
 }
