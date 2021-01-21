798185690d703916515ec5ab213cdbf203e2cebb
Kyungmo Ku <46951576+Kusin@users.noreply.github.com>
2021-01-21 17:08:57 +0900

    Update LCA@Dist.cpp

--- LCA@Dist.cpp
+++ LCA@Dist.cpp
@@ -2,43 +2,40 @@
 using namespace std;
 using ll = long long;
 const int N = 1e5+10, lN = 16; // lN = floor(log2(N-1)) + 1
-
-int par[lN+1][N],dep[N];
+int in[N],pv,par[N][lN],dep[N],val[N],sz[N];
 vector<int> g[N];
-
-void dfs(int k,int p=0){
-    int t=1,pp=p;   
-    dep[k] = dep[p] + 1;
-    par[0][k] = p;
-    while(par[t-1][pp]){
-        pp = par[t][k] = par[t-1][pp];
+void dfs(int k=1,int p=0){
+	in[k] = pv++;
+	dep[k] = dep[p]+1;
+    sz[k] = 1;
+    par[k][0] = p;
+    int pp = p, t = 1;
+    while(par[pp][t-1]){
+        pp = par[k][t] = par[pp][t-1];
         ++t;
     }
-    for(int nxt:g[k]) if(nxt!=p)
-        dfs(nxt,k);
+	for(int nxt:g[k])if(nxt!=p){
+		dfs(nxt,k);
+        sz[k] += sz[nxt];
+	}
+    //update smth
 }
-
 int LCA(int a,int b,int&dis){
     if(dep[a]<dep[b]) swap(a,b);
     int dif = dep[a] - dep[b];
     dis = dif;
-    for(int i=0;dif;dif>>=1,i++)
+    for(int i=0;dif;(dif>>=1),i++){
         if(dif&1)
-            a = par[i][a];
+            a = par[a][i];
+    }
     if(a==b) return a;
-    for(int i=lN;i>=0;i--)
-        if(par[i][a] != par[i][b]){
-            a=par[i][a]; b=par[i][b];
-            dis += 1<<i+1;
-        }
+    for(int i=lN-1;i>=0;i--)
+        if(par[a][i]!=par[b][i])
+            a = par[a][i], b = par[b][i] , dis += (1<<i+1);
     dis += 2;
-    return par[0][a];
+    return par[a][0];
 }
-
-int LCA(int a,int b){
-    int t;
-    return LCA(a,b,t);
-}
+int LCA(int a,int b){int t;return LCA(a,b,t);}=0){
 
 int main(){
     ios::sync_with_stdio(!cin.tie(0));
