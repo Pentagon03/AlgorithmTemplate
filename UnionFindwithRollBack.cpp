a14700143ed6ddd60771b85a3a86e2b64369d6ac
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2021-02-23 16:35:12 +0900

    Create UnionFindwithRollBack.cpp

---
+++ UnionFindwithRollBack.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const int N = 1e5+10;
+
+struct disjoint_set {
+	vector<int> par;
+  stack<tuple<int,int,int>> S;
+	disjoint_set(int n=N) : par(n+1, -1) {}
+	void init(int n=N) {
+		fill(par.begin(), par.begin()+n+1, -1);
+	}
+	int find(int u) {
+		return par[u] < 0 ? u : par[u] = find(par[u]);
+	}
+	bool merge(int u, int v) {
+		u = find(u), v = find(v);
+		if (u == v) return false;
+		if (par[u] < par[v]) swap(u, v); //then sz[u] < sz[v]
+    S.push({u, v, par[u]});
+		par[v] += par[u];
+		par[u] = v;
+		return true;
+	}
+  void rollback(){
+    if(S.empty()) return;
+    auto [u,v,sz] = S.top();
+    parent[v] -= sz;
+    parent[u] = sz;
+    S.pop();
+  }
+};
