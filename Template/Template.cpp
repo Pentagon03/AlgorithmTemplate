20534b8746aa5d78e480ce8ba808abe23fe39c5f
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2021-03-13 15:36:08 +0900

    Update Template.cpp

--- Template/Template.cpp
+++ Template/Template.cpp
@@ -6,36 +6,53 @@
 // ╚═╝░░░░░╚══════╝╚═╝░░╚══╝░░░╚═╝░░░╚═╝░░╚═╝░╚═════╝░░╚════╝░╚═╝░░╚══╝
 // Problem : 
 // bit.ly/pentagon03 
+
 #pragma GCC optimize("Ofast")
 #include<bits/stdc++.h>
 using namespace std;
+
 using ll = long long;
 using pii = pair<int,int>;
-using pll = pair<ll,ll>;
+using ti = tuple<int,int,int>;
 #define fi first
 #define se second
 
+// using C = double;
+// using P = pair<C,C>;
+// const double eps = 1e-9;
+// #define X first
+// #define Y second
+
 #define V(X) vector<X>
 #define pb push_back
-#define sz(v) ((int)(v).size())
+#define eb emplace_back
+#define ep emplace
 #define all(v) (v).begin(),(v).end() 
+#define sz(v) ((int)(v).size())
+#define compress(v) sort(all(v)),v.erase(unique(all(v)),v.end())
+
 #define rep(i,n) for(int i=0;i<(n);i++)
 #define rep1(i,n) for(int i=1;i<=(n);i++)
+#define repr(i,a,b) for(int i=(a);i<=(b);i++)
+#define repd(i,b,a) for(int i=(b);i>=(a);i--)
 
 #define nl '\n'
 #define sp ' '
-#define prt(x) cerr<<#x<<": "<<x<<nl
-void dbg(string s = "STILL WORKING"){puts(s.c_str());}
-
-template<typename T> T get_random_int(T l,T r) {
-	static random_device rd;
-	return uniform_int_distribution<T>(l,r)(rd);
-}
+#define yes(T) cout<<(T?"YES\n":"NO\n")
+#define pprt(x) cout<<(x)
+#define debug cerr<<"STILL WORKING\n";
+template<typename T> void __print(T n){cerr<<n;}
+template<typename T>
+void __print(const vector<T>&v) {int f = 0; cerr << '{'; for (auto &i: v) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#define dbg(x...) cerr << "[" << #x << "] = ["; _print(x)
 
 //Don't use FASTIO when u are handling real numbers
 #define FASTIO 0
 #if FASTIO
-namespace in{
+namespace IN{
 	const int SIZ=1<<20;
 	char buf[SIZ+1],*p=buf+SIZ;
 	inline bool isblank(char c){return c=='\n'||c==' '||c=='\t'||c=='\r'||c==0;}
@@ -61,7 +78,7 @@ namespace in{
 		scan(n); scan(args...);
 	}
 }
-namespace out{
+namespace OUT{
 	const int SIZ=1<<20;
 	char buf[SIZ+1],*p=buf,tmp[21];
 	inline void flush(){fwrite(buf,1,p-buf,stdout); p=buf;}
@@ -88,8 +105,8 @@ namespace out{
 	}
 	struct ff{ ~ff(){flush();}}flu;
 }
-using namespace in;
-using namespace out;
+using namespace IN;
+using namespace OUT;
 #else
 template<typename T=int> inline T geti(){T k; cin>>k; return k;}
 template<typename T> inline void scan(T&n){cin>>n;}
@@ -100,18 +117,31 @@ template<typename T>
 inline void write(T ans,char sep = '\n'){cout<<ans<<sep;}
 #endif
 
-const int N = 1e5+5, inf = 1e9, mod = 1e9+7;
+int TC = 1;
+bool getTC = 0, printTC = 0, useDouble = 0, fileIO = 0;
+const bool startup = [](){
+	#define ONLINE_JUDGE
+	#ifdef ONLINE_JUDGE
+	if(!FASTIO) ios::sync_with_stdio(!cin.tie(0)); 
+	#endif
+	if(fileIO) freopen("in.txt","r",stdin), 
+			   freopen("out.txt", "w", stdout);
+	if(getTC) cin>>TC;
+	if(useDouble) cout<<fixed<<setprecision(12);
+	return true;
+}();
+
 // int n;
 // vector<int> v(3);
 // void input(){
 // 	cin>>n; v.assign(n,0);
 // 	rep(i,n) cin>>v[i];
 // }
+
+const int N = 1e5+5, inf = 1e9, mod = 1e5+3;
+
 void solve(){
-	int n; scan(n);
-	V(int) v(n);
-	rep(i,n) cin>>v[i];
-	write(v);
+	int n,m,k; 
 }
 
 // did you initialize properly?
@@ -129,8 +159,6 @@ int main(){
 
 // Solution
 /*
-
-
 
 */
 
