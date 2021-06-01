074b5543dac10935f28f80289502b976f2373a74
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2021-06-01 19:31:06 +0900

    Update SegmentTree_ContinuousSum.cpp

--- SegmentTree_ContinuousSum.cpp
+++ SegmentTree_ContinuousSum.cpp
@@ -1,32 +1,18 @@
-//https://www.acmicpc.net/problem/16993
-#pragma GCC optimize("Ofast")
-//Always Do Your Best Ku!
-#include<bits/stdc++.h>
-using namespace std;
-#define all(v) (v).begin(),(v).end()
-#define sz(v) ((int)(v).size())
-#define nl '\n'
-#define sp ' '
-using ll = long long;
-using pii = pair<int,int>;
-const int inf = 1e9;
-#define dbg(X) cerr<<#X<<'='<<X<<nl
-#define GET_TC 0
-
+//Be aware that this tree is full binary tree
 template<typename T = ll,const T inf = T(1e18)>
 struct SegCon{
     struct Node{
         T mx, rmx, lmx, sum;
         Node(){mx = rmx = lmx = -inf; sum = 0;}
-        Node(T a,T b,T c,T d):mx(a),rmx(b),lmx(c),sum(d){}
+        Node(T v){mx = rmx = lmx = sum = v;}
     }base;
     vector<Node> tree; int b=1;
     SegCon(int n=1){
         while(b<n) b <<= 1;
         tree.resize(b<<1);
     }
-    void reset(){ tree.assign(b<<1,Node());}
-    Node f(Node a,Node b){
+    void reset(){tree.assign(b<<1,Node());}
+    Node f(const Node&a,const Node&b){
         Node res;
         res.sum = a.sum + b.sum;
         res.lmx = max(a.lmx,a.sum+b.lmx);
@@ -34,42 +20,19 @@ struct SegCon{
         res.mx = max({a.mx,b.mx,a.rmx+b.lmx});
         return res;
     }
-    Node init(int nd,int s,int e,vector<T>&v){
-        if(s+1==e)
-            return tree[nd] = Node(v[s],v[s],v[s],v[s]);
-        int m = s + e >> 1;
-        return tree[nd] = f(init(nd<<1,s,m,v),init(nd<<1|1,m,e,v));
-    } void init(vector<T>&v){ v.resize(b); init(1,0,b,v);}
-
-    void upd(int p,T plus){
-        T v = tree[p+=b].sum;  v += plus;
-        for(tree[p]=Node(v,v,v,v);p>>=1;)
-            tree[p] = f(tree[p<<1],tree[p<<1|1]);
-    } 
-
-    Node query(int nd,int s,int e,int l,int r){
-        if(e<=l || r<=s) return base;
-        if(l<=s && e<=r) return tree[nd];
-        int m = s + e >> 1;
-        return f(query(nd<<1,s,m,l,r),query(nd<<1|1,m,e,l,r));
-    } T query(int l,int r){return query(1,0,b,l,r).mx;}
-};
-
-
-void solve(int TC){
-    int n; cin>>n;
-    SegCon tree(n); 
-    vector<ll> v(n); for(int i=0;i<n;i++) cin>>v[i];
-    tree.init(v);
-    int Q; cin>>Q;
-    while(Q--){
-        int a,b; cin>>a>>b;
-        cout<<tree.query(--a,b)<<nl;
+    void init(vector<T>&v){
+        *this = SegCon(v.size()); 
+        for(int i=0;i<v.size();i++) tree[b+i] = Node(v[i]);
+        for(int i=b-1;i>=1;i--) tree[i] = f(tree[i<<1],tree[i<<1|1]);
     }
-}
-
-int main(){
-    ios::sync_with_stdio(!cin.tie(0));
-    int T = 1; if(GET_TC) cin >> T;
-    for(int i=1;i<=T;i++) solve(i);
-}
+    void upd(int p,T plus){
+        if(!plus) return;
+        T v = tree[p+=b].sum;  v += plus;
+        for(tree[p]=Node(v);p>>=1;) tree[p] = f(tree[p<<1],tree[p<<1|1]);
+    } 
+    Node qry(int nd,int s,int e,int l,int r){
+        if(e<l || r<s) return base;
+        if(l<=s && e<=r) return tree[nd];
+        return f(qry(nd<<1,s,s+e>>1,l,r),qry(nd<<1|1,s+e+2>>1,e,l,r));
+    } T qry(int l,int r){return qry(1,0,b-1,l,r).mx;}
+};
