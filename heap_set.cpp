6f8c0263b48a9a239dae0778ecc1ca15757bc965
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2021-06-03 10:03:59 +0900

    Create heap_set.cpp

---
+++ heap_set.cpp
@@ -0,0 +1,21 @@
+//first:val, second:id
+struct heap_set{
+	priority_queue<pii> iq,dq;
+	void insert(pii x){ iq.push(x); }
+	void del(pii x){ dq.push(x); }
+	void upd(){
+		while(!dq.empty()){
+			if(!iq.empty()) dq.pop();
+			else{
+				if(dq.top().first > iq.top().first) dq.pop();
+				else if(dq.top().first == iq.top().first) dq.pop(), iq.pop();
+				else break;
+			}
+		}
+	}
+	bool empty(){upd(); return iq.empty();}
+	pii top(){
+		if(!empty()) return iq.top();
+		return pii(-1,-1);
+	}
+};
