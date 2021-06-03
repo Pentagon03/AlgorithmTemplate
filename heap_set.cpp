d038fe3256bc78f354eead60737ef18b8feee35d
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2021-06-03 11:14:39 +0900

    Update heap_set.cpp

--- heap_set.cpp
+++ heap_set.cpp
@@ -1,22 +1,32 @@
 //first:val, second:id
+struct Data{
+	int val, id;
+	Data(int a=-1,int b=-1){val=a;id=b;}
+	bool operator<(const Data x) const{
+		return val>x.val;
+	}
+	bool operator==(const Data x) const{
+		return val==x.val;
+	}
+};
 struct heap_set{
-	priority_queue<pii> iq,dq;
-	void push(pii x){ iq.push(x); }
-	void del(pii x){ dq.push(x); }
+	priority_queue<Data> iq,dq;
+	void push(Data x){ iq.push(x); }
+	void del(Data x){ dq.push(x); }
 	void upd(){
 		while(!dq.empty()){
-			if(!iq.empty()) dq.pop();
+			if(iq.empty()) dq.pop();
 			else{
-				if(dq.top().first > iq.top().first) dq.pop();
-				else if(dq.top().first == iq.top().first) dq.pop(), iq.pop();
+				if(iq.top()<dq.top()) dq.pop();
+				else if(iq.top()==dq.top()) dq.pop(), iq.pop();
 				else break;
 			}
 		}
 	}
 	bool empty(){upd(); return iq.empty();}
 	void pop(){if(!empty()) iq.pop();}
-	pii top(){
+	Data top(){
 		if(!empty()) return iq.top();
-		return pii(-1,-1);
+		return Data();
 	}
 };
