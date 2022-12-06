784c24ca9adeafd13384d3a9cb4e6a5a2f0c5cbd
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2022-12-07 03:40:09 +0900

    Update fastLCS.cpp

--- fastLCS.cpp
+++ fastLCS.cpp
@@ -1,24 +1,27 @@
-string A,B;
-const int MX = 5e4 + 10;
+string A, B;
+const int MX = 1e4 + 10;
 const int ALP = 26;
 using ull = unsigned long long;
-ull dt[(MX>>6)+1],S[ALP][(MX>>6)+1];
-
+ull dt[(MX>>6)+1], S[ALP][(MX>>6)+1];
 #include<immintrin.h>
-void solve(){
-    cin>>A>>B;
+vector<int> getLCSdp(string&A,string&B){
 	int n=A.length(),m=B.length(),ans=0;
-	for(int i=0;i<m;i++) S[B[i]-'A'][i>>6]|=1ULL<<(i&63);
-	int t = (m>>6)+1;
+	int siz = (m>>6)+1;
+
+	// Init
+	for(int i=0;i<siz;i++) dt[i] = 0;
+	for(int i=0;i<ALP;i++) for(int j=0;j<siz;j++) S[i][j] = 0;
+	
+	for(int i=0;i<m;i++) S[B[i]-'a'][i>>6]|=1ULL<<(i&63);
 	for(int i=0;i<n;i++){
-		for(int j=0, c=1, mc=0; j<t;j++){
-			ull x = dt[j] | S[A[i]-'A'][j], y;
+		for(int j=0, c=1, mc=0; j<siz;j++){
+			ull x = dt[j] | S[A[i]-'a'][j], y;
 			mc = _subborrow_u64(mc, x, (dt[j]<<1)|c, &y);
             c = dt[j] >> 63;
 			dt[j] = x & (x^y);
 		}
 	}
-	for(int i=0;i<t;i++) ans += __builtin_popcountll(dt[i]);
-	// for(int i=0;i<m;i++) ans += !!(dt[i>>6] & (1ULL<<(i&63)));
-	cout<<ans<<nl;
+    vector<int> v(m+1);
+	rep(i,m) v[i+1] = v[i] + !!(dt[i>>6] & (1ULL<<(i&63)));
+	return v;
 }
