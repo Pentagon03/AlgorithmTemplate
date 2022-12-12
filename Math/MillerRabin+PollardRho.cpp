4e851f8fe6803a141b9054394d5fd134c478820d
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2022-12-12 21:17:42 +0900

    Update and rename MillerRabin.cpp to MillerRabin+PollardRho.cpp

--- Math/MillerRabin+PollardRho.cpp
+++ Math/MillerRabin+PollardRho.cpp
@@ -1,7 +1,7 @@
 using ull = unsigned long long;
 constexpr int SZ = 100000; 
 bitset<SZ> isp; vector<int> prlist;
-void Sieve(){
+bool Sieve(){
     isp.set(); isp[0]=isp[1]=false;
     for(int i=2;i<SZ;i++){
         if(isp[i]){
@@ -10,6 +10,7 @@ void Sieve(){
             for(int j=i*i;j<SZ;j+=i) isp[j] = false;
         }
     }
+    return true;
 }
 ull MulMod(ull a,ull b,ull c){return (__uint128_t)a*b%c;}
 ull PowMod(ull a,ull b,ull c){
@@ -31,9 +32,40 @@ bool MillerRabin(ull n, ull a){ // is n prime based on a
     return false;
 }
 bool IsPrime(ull n){
+    static bool start = Sieve();
     if(n<SZ) return isp[n];
     // check some base primes
     for(int i=0;i<5;i++) if(n%prlist[i]==0) return false;
     for(int p:p64) if(!MillerRabin(n,p)) return false;
     return true;
+}
+template<typename T> T get_random(T l, T r){
+    static random_device rd;
+    return uniform_int_distribution<T>(l,r)(rd);
+}
+ull Rho(ull n){
+    while(true){
+        ull x_k = get_random<ull>(2,n-1), x_2k = x_k, c = get_random<ull>(1,n-1);
+        auto f = [&](ull x){
+            return (MulMod(x,x,n)+c)%n;
+        };
+        while(true){
+            x_k = f(x_k); x_2k = f(f(x_2k));
+            ull p = gcd(abs((ll)x_2k-(ll)x_k), n); if(p==1) continue;
+            if(IsPrime(p)) return p;
+            else{ n = p; break;}
+        }
+    }
+}
+vector<pair<ull,ull>> Factorize(ull n){
+    vector<pair<ull,ull>> v;
+    int two = __builtin_ctzll(n);
+    if(two>0) v.emplace_back(2, two), n >>= two;
+    if(n<=1) return v;
+    while(!IsPrime(n)){
+        ull p = Rho(n), cnt = 0; while(n%p==0) n/=p, cnt++;
+        v.emplace_back(p, cnt); if(n==1) break;
+    }
+    if(n>1) v.emplace_back(n,1);
+    return v;
 }
