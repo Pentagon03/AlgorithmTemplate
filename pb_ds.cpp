5f0fd856bf946235fcd963d0115ef4194d8a0e97
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2023-03-17 23:31:20 +0900

    Update pb_ds.cpp

--- pb_ds.cpp
+++ pb_ds.cpp
@@ -2,9 +2,9 @@
 using namespace __gnu_pbds;
 using ordered_set=tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update>;
 using ordered_multiset=tree<int, null_type, less_equal<int>, rb_tree_tag,tree_order_statistics_node_update>;
+// for multiset, function of upper_bound and lower_bound swap.
 
 /*
 os.find_by_order(integer) => iterator
 os.order_of_key(key) => integer
-
 */
