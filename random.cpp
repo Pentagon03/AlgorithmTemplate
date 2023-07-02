27e470bb66920a6df9327e42aa015461d234d956
AlgorithmLegend <110778085+AlgorithmLegend@users.noreply.github.com>
2023-07-02 16:12:15 +0900

    Update random.cpp

--- random.cpp
+++ random.cpp
@@ -1,5 +1,7 @@
-template<typename T=int> T get_random(T l,T r) {
+//[l, r]
+template<typename T=int> T get_random_int(T l,T r) {
 	//mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+	// return uniform_int_distribution<T>(l,r)(rng);
 	static random_device rd;
 	return uniform_int_distribution<T>(l,r)(rd);
 }
