9294b406ec6c17e3c56195b08c6b6ffce6c349a2
Kyungmo Ku <46951576+Pentagon03@users.noreply.github.com>
2024-02-02 16:39:18 +0900

    Update Geometry.cpp

--- Preparation/Geometry.cpp
+++ Preparation/Geometry.cpp
@@ -1,4 +1,4 @@
-amespace Geometry{
+namespace Geometry{
     using C = double;
     const C EPS = 1e-12;
     const C PI = acos(-1);
@@ -7,15 +7,15 @@ amespace Geometry{
     #define fi first
     #define se second
     const P O = P(0,0), FAIL = P(numeric_limits<T>::max(), numeric_limits<T>::max());
-    inline T inner(P a,P b){ return a.fi*b.fi+ a.se*b.se;} //inner product
-    inline T cross(P a,P b){ return a.fi*b.se-a.se*b.fi;} //vector product
+    inline T inner(P a,P b){ return a.fi*b.fi + a.se*b.se;} //inner product
+    inline T cross(P a,P b){ return a.fi*b.se - a.se*b.fi;} //vector product
     istream& operator>> (istream& is, P& p){ return is >> p.fi >> p.se;}
     inline P operator+(P a,P b){ return P(a.fi+b.fi, a.se+b.se);}
     inline P operator-(P a,P b){ return P(a.fi-b.fi, a.se-b.se);}
-    inline P operator*(int c,P a){ return P(a.fi*c,a.se*c); }
-    inline P operator*=(P&a,int c){ return a = c*a; }
+    inline P operator*(P a,T c){ return P(a.fi*c,a.se*c); }
+    inline P operator*=(P &a, T c){ return a = a*c; }
     inline P operator-=(P&a,P b){ return a = a-b; }
-    inline T sq(P a){return a.fi*a.fi+a.se*a.se;}
+    inline T sq(P a){return inner(a,a);}
     inline T dsq(P a,P b){return sq(b-a);}
     inline int ccw(P a,P b,P c){T val = cross(b-a,c-a); return val?(val>0?1:-1):0;}
     inline bool cmp_ang(P a,P b){
@@ -37,10 +37,9 @@ amespace Geometry{
             }
             return t1*t2 <= 0 && t3*t4 <= 0;
         }
-        friend P parrallel_inter(Line p,Line q){
-            P&a=p.s,&b=p.e,&c=q.s,&d=q.e;
-            if(b==c) return b;
-            if(d==a) return a;
+        friend P parrallel_inter(Line a,Line b){
+            if(a.e==b.s) return a.e;
+            if(b.e==a.s) return b.e;
             return FAIL;
         }
         // only works when T = double
@@ -51,12 +50,13 @@ amespace Geometry{
                 return parrallel_inter(p,q);
             b-=a; d-=c;
             C t = cross(c-a,d) / cross(b,d);
-            return a + t*b;
+            return a + b*t;
         }
         friend C dist(Line a,P x){ return abs(cross(a.e-a.s,x-a.s))/a.length(); }
     };
+
     using Polygon = vector<P>;
-    Polygon getCH(const Polygon&pg){ //Monotone Chain of polygon
+    Polygon getCH(Polygon pg){ //Monotone Chain of polygon
         if(size(pg)<=3) return pg;
         sort(all(pg));
         Polygon up(size(pg)),dn(size(pg)); int u=0,d=0;
@@ -69,7 +69,7 @@ amespace Geometry{
         dn.insert(dn.end(),++up.rbegin(),--up.rend());
         return dn;
     }
-    bool inner_naive(const Polygon&h, P p){
+    bool chk_in_naive(const Polygon&h, P p){
         // strictly inner
         int s = ccw(h[0],h[1],p);
         for(int i=1;i<size(h);i++){
@@ -78,5 +78,4 @@ amespace Geometry{
         return true;
     }
 }
-
 using namespace Geometry;
